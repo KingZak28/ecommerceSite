e2efed4560c66e4a917ef68bc849439bb58b3be7
KingZak28 <zakariah28@hotmail.com>
2020-09-04 07:54:13 -0400

    Did some work on the main landing page of the ecommerce application

---
+++ ecommerce-site/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import MainPage from "./mainPage";
+
+function App() {
+  return (
+    <div>
+      <MainPage></MainPage>
+    </div>
+  );
+}
+
+export default App;
