e9f280dcd8316335cc4a64088c79eca9e54b8a9e
KingZak28 <zakariah28@hotmail.com>
2020-09-08 13:58:46 -0400

    Added the items into the cart dropdown menu

--- ecommerce-site/src/components/cartDropDown/cartDropDown.js
+++ ecommerce-site/src/components/cartDropDown/cartDropDown.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
 import PrettyButton from "../prettyButton/prettyButton";
+import CartItem from "../cartItem/cartItem";
 import "./cartDropDown.scss";
 
-const CartDropDown = () => (
+const CartDropDown = ({ cartItems }) => (
   <div className="cartDropDown">
     <div className="cartItems" />
+    {cartItems.map((item) => (
+      <CartItem key={item.id} item={item}></CartItem>
+    ))}
     <PrettyButton>CHECKOUT</PrettyButton>
   </div>
 );
 
-export default CartDropDown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropDown);
