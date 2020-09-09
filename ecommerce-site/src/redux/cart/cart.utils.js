0a460c34790b560e9ec04f17aae579aa97c033fe
KingZak28 <zakariah28@hotmail.com>
2020-09-09 15:03:16 -0400

    Added the ability to add and remove items from the cart

--- ecommerce-site/src/redux/cart/cart.utils.js
+++ ecommerce-site/src/redux/cart/cart.utils.js
@@ -1,9 +1,9 @@
-export const addItemToCart = (cartItems, cartItemToAdd) => {
-  const exisitingCartItem = cartItems.find(
-    (cartItem) => cartItem.id === cartItemToAdd.id
-  );
+const exisitingCartItem = (cartItems, cartItemToSearch) => {
+  return cartItems.find((cartItem) => cartItem.id === cartItemToSearch.id);
+};
 
-  if (exisitingCartItem) {
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  if (exisitingCartItem(cartItems, cartItemToAdd)) {
     // We need to return a new array so the component rerenders
     return cartItems.map((cartItem) =>
       cartItem.id === cartItemToAdd.id
@@ -14,4 +14,20 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   // If its not found we can add the cart items but each item is initialized with a quantity of 1.
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const itemInCart = exisitingCartItem(cartItems, cartItemToRemove);
+
+  //Remove items once this is called and it is at 1.
+  if (itemInCart.quantity === 1) {
+    // Keep the items that arent equal to the cart item to remove
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
 };
