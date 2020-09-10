6329920c6f2fb4547eaaad27fee34810874209ba
KingZak28 <zakariah28@hotmail.com>
2020-09-10 12:52:54 -0400

    Added persistance to the app through the use of local storage and the redux-persist library

--- ecommerce-site/src/redux/rootReducer.js
+++ ecommerce-site/src/redux/rootReducer.js
@@ -1,8 +1,19 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // local storage
 import userReducer from "./user/userReducer";
 import cartReducer from "./cart/cartReducer";
 
-export default combineReducers({
+const persistConfig = {
+  // the key is the point at which we want to start storing things and the white list is a list of reducers we want to persist
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
