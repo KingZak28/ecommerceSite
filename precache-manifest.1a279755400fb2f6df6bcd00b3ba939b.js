3f9b57e92d8e68cda6c653d21b7da470b8f3c075
KingZak28 <zakariah28@hotmail.com>
2020-09-12 14:25:57 -0400

    Updates

---
+++ precache-manifest.1a279755400fb2f6df6bcd00b3ba939b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40bff8890df30ad67f52fbe5a41bd3d1",
+    "url": "/ecommerceSite/index.html"
+  },
+  {
+    "revision": "27ef322aab9157f1aa02",
+    "url": "/ecommerceSite/static/css/main.8406cb8f.chunk.css"
+  },
+  {
+    "revision": "ee6a9df1226822bc6ab8",
+    "url": "/ecommerceSite/static/js/2.ff86cdac.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/ecommerceSite/static/js/2.ff86cdac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ef322aab9157f1aa02",
+    "url": "/ecommerceSite/static/js/main.59a6653b.chunk.js"
+  },
+  {
+    "revision": "c50f3e11fbf4123271c7",
+    "url": "/ecommerceSite/static/js/runtime-main.cd014d99.js"
+  },
+  {
+    "revision": "465e70e7824c70915d0fd88586cbe678",
+    "url": "/ecommerceSite/static/media/bag.465e70e7.svg"
+  },
+  {
+    "revision": "6fa537974f535b1c08a17324cb440605",
+    "url": "/ecommerceSite/static/media/new.6fa53797.svg"
+  }
+]);
